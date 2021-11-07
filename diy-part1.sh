c89c38bcf19670664657a434cb1e900bc3317fdd
zxing <0xHO>
2021-11-07 18:28:59 +0800

    add ddnsto xray

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,10 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 echo 'src-git xray https://github.com/yichya/openwrt-xray.git;master' >> feeds.conf.default
 echo 'src-git xray_luci https://github.com/yichya/luci-app-xray.git;master' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr.git;master' >> feeds.conf.default
